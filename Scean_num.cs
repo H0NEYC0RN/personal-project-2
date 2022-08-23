ea5c22ddb65eed5fd3f56e5128ac883c2633ad70
C078056 허지수 <87184810+H0NEYC0RN@users.noreply.github.com>
2022-08-23 16:21:29 +0900

    이름 변경

--- Scean_num.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Scean_num : MonoBehaviour
-{
-    public int SceanNum;
-    // 1 - Shop / 2 - Adventure / 3 - Main
-}
